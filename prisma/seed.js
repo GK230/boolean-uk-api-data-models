4ab1e425596dcb984a4b3ccc912accf44691845a
Geeta Kotecha <geeta.kotecha@gmail.com>
2021-08-12 17:07:17 +0100

    created schema and mock data

---
+++ prisma/seed.js
@@ -0,0 +1,137 @@
+const designers = [
+  {
+    firstName: "Marc",
+    lastName: "Jacobs",
+  },
+  {
+    firstName: "Geeta",
+    lastName: "Kotecha",
+  },
+  {
+    firstName: "Victoria",
+    lastName: "Beckhan",
+  },
+  {
+    firstName: "Vivenne",
+    lastName: "Westwood",
+  },
+  {
+    firstName: "Stella",
+    lastName: "McCartney",
+  },
+  {
+    firstName: "Yoji",
+    lastName: "Yamamoto",
+  },
+];
+
+const events = [
+  {
+    location: "Paris",
+  },
+  {
+    location: "London",
+  },
+  {
+    location: "Milan",
+  },
+  {
+    location: "New York",
+  },
+];
+
+const guests = [
+  {
+    firstName: "Princess",
+    lastName: "Kate",
+  },
+  {
+    firstName: "Gwenth",
+    lastName: "Paltrow",
+  },
+  {
+    firstName: "Anna",
+    lastName: "Wintour",
+  },
+  {
+    firstName: "Priyanka",
+    lastName: "Chopra-Jonas",
+  },
+  {
+    firstName: "Edward",
+    lastName: "Enninful",
+  },
+];
+
+const models = [
+  {
+    firstName: "Cara",
+    lastName: "Delevinge",
+  },
+  {
+    firstName: "Gigi",
+    lastName: "Hadid",
+  },
+  {
+    firstName: "Bella",
+    lastName: "Hadid",
+  },
+  {
+    firstName: "Kate",
+    lastName: "Moss",
+  },
+  {
+    firstName: "Liu",
+    lastName: "Wen",
+  },
+];
+const outfits = [
+  {
+    description: "evening gown",
+    season: "SS",
+  },
+  {
+    description: "coat",
+    season: "AW",
+  },
+  {
+    description: "jumpsuit",
+    season: "SS",
+  },
+  {
+    description: "hat",
+    season: "AW",
+  },
+  {
+    description: "hat",
+    season: "SS",
+  },
+  {
+    description: "evening gown",
+    season: "AW",
+  },
+  {
+    description: "evening gown",
+    season: "SS",
+  },
+  {
+    description: "coat",
+    season: "AW",
+  },
+  {
+    description: "jumpsuit",
+    season: "SS",
+  },
+  {
+    description: "hat",
+    season: "AW",
+  },
+  {
+    description: "hat",
+    season: "SS",
+  },
+  {
+    description: "evening gown",
+    season: "AW",
+  },
+];
